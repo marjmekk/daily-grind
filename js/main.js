25681cbde47b4b2db3a1a33d3f55ef886c4b0a00
Marjorie <marjorie.mekk@seattlecolleges.edu>
2022-11-30 21:11:24 -0800

    daily-grind

--- js/main.js
+++ js/main.js
@@ -29,9 +29,9 @@ function coffeeTemplate(coffee) {
     alt="${coffee.alt}" 
     id="coffee" />
 <strong class"feature">${coffee.day}'s Coffee Special:</strong> ${coffee.day}'s daily coffee special 
-${coffee.name}.
-${coffee.desc}</p>`;
-
+<b>${coffee.name}</b>.
+${coffee.desc}</p>
+<p><strong>Quote of the day:</strong> ${coffee.quote}</p>`;
 }
 
 let myDate = new Date ();
@@ -67,7 +67,8 @@ switch(parseInt(myDay)){
             pic: "mocha.jpg",
             alt: "A picture of a Mocha",
             day: "Sunday",
-            desc: 'You mocha me very happy:'
+            desc: 'You mocha me very happy: Mocha coffee tastes like a chocolate-y flavor. It is more bitterness of the espresso shot combined with the sweetness of the chocolate and your choices of milk.',
+            quote: "Sunday clears away the rust of the whole week.~ Joseph Addison"
         };
     break;
 
@@ -79,7 +80,8 @@ switch(parseInt(myDay)){
             pic: "bubble-tea.jpg",
             alt: "A picture of a bubble tea",
             day: "Monday",
-            desc: 'I like me some Bubble Tea!:'
+            desc: 'I like me some Bubble Tea!: A cold, frothy, drink made with a tea base shaken or blended with flavors, sweeteners,and/or milk with your choice of topping at the base of the drink. Do not forget to mix with together!',
+            quote: "This is your Monday morning reminder that you can handle whatever this week throws at you ~ unknown"
         };
     break;
 
@@ -91,7 +93,8 @@ switch(parseInt(myDay)){
             pic: "caramel-latte.jpg",
             alt: "A picture of a caramel latte",
             day: "Tuesday",
-            desc:"It's cold, so a caramel latte sounds good:"
+            desc:"It's cold, so a caramel latte sounds good: It's Mixing with espresso shots with caramel syrup and pouring steamed milk on the top! You can use any choices of milk and drink it cold or hot.",
+            quote: "Tuesday isn't the happiness day, but it may be the healthiest ~Unknown"
         };
         break; 
 
@@ -103,7 +106,8 @@ switch(parseInt(myDay)){
                 pic: "cold-brew.jpg",
                 alt: "A picture of a cold brew",
                 day: "Wednesday",
-                desc:"Cold brew for when we're serious!:"
+                desc:"Cold brew for when we're serious!: It is literally a concentrated coffee drink and is much stronger than drip coffee.",
+                quote: "Win-This-Day!"
             };
             break; 
 
@@ -115,7 +119,8 @@ switch(parseInt(myDay)){
                     pic: "drip.jpg",
                     alt: "A picture of a drip",
                     day: "Thursday",
-                    desc: "Drip coffee for basic coffee drinker:"
+                    desc: "Drip coffee for basic coffee drinker: You can make it at home or go any local coffee stand. All you need is a boiling hot water over ground coffee. So Simple!",
+                    quote: "Some people call it Thursday. I like to call it Friday Eve!"
                 };
             break;
 
@@ -123,11 +128,12 @@ switch(parseInt(myDay)){
                 today = "Friday"; 
                 coffee = {
                     color:"#A3C7D6",
-                    name: "Frappaccino",
+                    name: "Frappuccino",
                     pic: "frappaccino.jpg",
-                    alt: "A picture of a frappaccino",
+                    alt: "A picture of a frappuccino",
                     day: "Friday",
-                    desc: 'Frappaccino for treat yourself day!:'
+                    desc: 'Frappuccino for treat yourself day! It consists of with coffee or creme base, blended with ice and ingredients such as flavored syrups and topped with whipped cream and/or spices.',
+                    quote: "Friday afternoon feels like Heaven! ~El Fuego"
                 };
             break;
 
@@ -139,7 +145,8 @@ switch(parseInt(myDay)){
                     pic: "pumpkin-spice-latte.jpg",
                     alt: "A picture of a pumpkin spice latte",
                     day: "Saturday",
-                    desc: 'Fragrant with notes of cinnamon, clove, and nutmug this fall classic is a sweet and cozy reminder of the season:'
+                    desc: 'Fragrant with notes of cinnamon, clove, and nutmug this fall classic is a sweet and cozy reminder of the season:',
+                    quote: "It's Saturday - enjoy the way with the ones you love. Chill and relax!"
                 };
             break;
 
